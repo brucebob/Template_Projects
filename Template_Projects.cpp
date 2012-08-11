2576570438e3b841403c0af4d358de4820033e84
Bruce <brucebob1@gmail.com>
2012-08-11 05:16:39 -0400

    Moved the tests to the UnitTesting.cpp

--- Template_Projects.cpp
+++ Template_Projects.cpp
@@ -22,7 +22,6 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	try
 	{
-		TEST_MAIN();
 		srand(0);
 		MemoryTracker mt;
 		
@@ -61,63 +60,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	return 0;
 }
-
-TEST_ADD(MergeTest)
-{
-	SortedList<int> list1;
-	SortedList<int> list2;
-	for(unsigned int x = 1; x < 10; x += 2)
-	{
-		list1.add(x);
-	}
-	for(unsigned int x = 0; x < 10; x += 2)
-	{
-		list2.add(x);
-	}
-	list1.merge(list2);
-	for(unsigned int x = 0; x < 10; x++)
-	{
-		TEST_CONDITION(x == list1[x]);
-	}
-	TEST_CONDITION(list2.isEmpty() == true);
-}
-TEST_ADD(VectorToSortedList)
-{
-	std::vector<int> vList;
-	for(unsigned int x = 0; x < 10; x++)
-	{
-		vList.push_back(x);
-	}
-	SortedList<int> sList(vList);
-
-	for(int x = 0; x < 10; x++)
-	{
-		TEST_CONDITION(sList[x] == x);
-	}
-}
-TEST_ADD(SortedElements)
-{
-	
-	int randomNum = 10;
-
-	SortedList<int> sList;
-	std::vector<int> vList;
-	for(int x = 0; x < randomNum; x++)
-	{
-		int y = (rand() % 5000) - 2500;
-		vList.push_back(y);
-		sList.add(y);
-	}
-	std::sort(vList.begin(), vList.end());
-	for(int x = 0; x < vList.size(); x++)
-	{
-		TEST_CONDITION(vList[x] == sList[x]);
-	}
-}
-
-
-
-
 
 /*
 #pragma once
---
+++ UnitTesting.cpp
@@ -0,0 +1,60 @@
+#include "TestSuite.h"
+
+#include "List.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+TEST_ADD(SortedList_MergeTest)
+{
+	SortedList<int> list1;
+	SortedList<int> list2;
+	for(unsigned int x = 1; x < 10; x += 2)
+	{
+		list1.add(x);
+	}
+	for(unsigned int x = 0; x < 10; x += 2)
+	{
+		list2.add(x);
+	}
+	list1.merge(list2);
+	for(unsigned int x = 0; x < 10; x++)
+	{
+		TEST_CONDITION(x == list1[x]);
+	}
+	TEST_CONDITION(list2.isEmpty() == true);
+}
+TEST_ADD(SortedList_VectorToSortedList)
+{
+	std::vector<int> vList;
+	for(unsigned int x = 0; x < 10; x++)
+	{
+		vList.push_back(x);
+	}
+	SortedList<int> sList(vList);
+
+	for(int x = 0; x < 10; x++)
+	{
+		TEST_CONDITION(sList[x] == x);
+	}
+}
+TEST_ADD(SortedList_SortedElements)
+{
+
+	int randomNum = 10;
+
+	SortedList<int> sList;
+	std::vector<int> vList;
+	for(int x = 0; x < randomNum; x++)
+	{
+		int y = (rand() % 5000) - 2500;
+		vList.push_back(y);
+		sList.add(y);
+	}
+	std::sort(vList.begin(), vList.end());
+	for(int x = 0; x < vList.size(); x++)
+	{
+		TEST_CONDITION(vList[x] == sList[x]);
+	}
+}
