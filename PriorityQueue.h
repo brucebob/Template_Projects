eebf71de134cc157f64a07a979bfd31359547493
Bruce <brucebob1@gmail.com>
2012-08-16 22:48:59 -0400

    add shell functions to be fleshed out later

---
+++ PriorityQueue.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+
+template<typename T>
+class PriorityQueue
+{
+private:
+	std::vector<T> que;
+	void perulateUp(int spot, int spot2)
+	{
+
+	}
+public:
+	void push(const T& t)
+	{
+
+	}
+	void push(T && t)
+	{
+
+	}
+
+};
--- Template_Projects.cpp
+++ Template_Projects.cpp
@@ -15,43 +15,12 @@
 #include "TestSuite.h"
 #include "Exports/BSTree.h"
 
-
-using std::string;
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	try
 	{
 		TEST_MAIN();
-		MemoryTracker mt;
 		
-		StopWatch t;
-		double total = 0;
-		for(unsigned int w = 0; w < 10; w++)
-		{
-			for(unsigned int y = 0; y < 10; y++)
-			{
-				SortedList<int> sList;
-				t.StartTimer();
-				for(unsigned int x = 0; x < 10000; x++)
-				{
-					sList.add( (rand() % 5000) - 2500);
-				}
-				t.EndTimer();
-				std::cout.precision(3);
-				total = t.ElapsedTime();
-				std::cout << t.ElapsedTime() << std::endl;
-			}
-			printf("%f took this long\n", total / 10);
-			total = 0;
-		}
-
-		// memory testors.
-		/*
-		mt.FirstState();
-		mt.SecondState();
-		mt.Print(std::cout);
-		*/
 	}
 	catch (const std::exception& e)
 	{
